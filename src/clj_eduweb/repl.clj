0c4dc1035aa76623fd26188e7982a367d8e66168
Yurii <tovy@1c.ru>
2020-01-29 17:15:26 +0300

    add aliases for namespaces

--- src/clj_eduweb/repl.clj
+++ src/clj_eduweb/repl.clj
@@ -1,8 +1,7 @@
 (ns clj-eduweb.repl
   (:require [clj-eduweb.core :refer :all]
             [clj-eduweb.elements :refer :all]
-            [clj-eduweb.journal :refer :all]
-            [clj-eduweb.administration :refer :all]
+            [clj-eduweb.journal :as journal]
+            [clj-eduweb.administration :as administration]
             [clojure.repl :refer [dir doc source apropos pst]]
-            [clojure.test :refer [deftest are is run-tests]]
             [clojure.java.io :as io]))
