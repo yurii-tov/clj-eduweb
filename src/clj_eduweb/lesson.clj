aa2a5b8c90847d7d36f6b353c1a0ce0b1b8f6ae5
Yurii <juretz32@yandex.ru>
2020-06-17 22:34:28 +0300

    Lesson-oriented API

--- src/clj_eduweb/lesson.clj
+++ src/clj_eduweb/lesson.clj
@@ -11,6 +11,28 @@
        (catch Exception e)))
 
 
+(defn complete []
+  (click (find-element (css "#e4-library-LessonPanel-closeButton")))
+  (get-text (find-element (css ".lesson-result-score-value"))))
+
+
+(defn start []
+  (click (find-element (css "#btnStartLesson"))))
+
+
+(defn start-again []
+  "Start from results page"
+  (click (find-element (css "#e4-library-LessonStatsPanel-StartAgainButton")))
+  (start))
+
+
+(defn complete-and-restart []
+  (complete)
+  (start-again))
+
+
+;; Content testing
+
 
 (defn store-answer []
   "Store answer for current qti question, jump to the next one"
@@ -24,5 +46,15 @@
   "Plug stored answer into current question, jump to the next one"
   (let [frame (qti/find-qti-frame)]
     (qti/with-qti-frame frame
-      (qti/fill-answer))
+      (qti/fill-answer)
+      (qti/submit))
     (move-forward frame)))
+
+
+(defn store-answers []
+  (while (store-answer)))
+
+
+(defn fill-answers []
+  (while (fill-answer))
+  (complete))
