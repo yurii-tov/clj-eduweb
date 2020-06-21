074f014fbcc29a8f20fe639ba521374ecf6739cf
Yurii <juretz32@yandex.ru>
2020-06-21 19:22:33 +0300

    Add base sanity check for project integrity

---
+++ test/clj_eduweb/integrity_test.clj
@@ -0,0 +1,12 @@
+(ns clj-eduweb.integrity-test
+  (:require [clj-eduweb.core :as core]
+            [clj-eduweb.elements :as elements]
+            [clj-eduweb.qti :as qti]
+            [clj-eduweb.lesson :as lesson]
+            [clj-eduweb.administration :as administration]
+            [clj-eduweb.repl :as repl]
+            [clojure.test :refer :all]))
+
+
+(deftest all-compiling
+  (println "All ns compiling successfully"))
